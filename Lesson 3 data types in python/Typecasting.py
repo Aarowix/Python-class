6aaf8c4b879a78de57f7e9c2f232b324f7d78901
Aarowix <aarowixthesecond@gmail.com>
2025-12-28 15:44:30 +0400

    Typecasting

---
+++ Lesson 3 data types in python/Typecasting.py
@@ -0,0 +1,18 @@
+name = "Inko"
+age = 9
+is_student = True
+weight = 23
+print("Name:", name )
+print("type of name", type(name))
+print("age:", age )
+print("type of age", type(age))
+print("is_student:", is_student)
+print("type of is_student", type(is_student))
+print("weight", weight )
+print("type of weight", type(weight))
+age = str(age)
+print(age)
+print("type of age :", type(age))
+weight = int(weight)
+print(weight)
+print("type of weight :", type(weight))
