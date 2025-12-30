dbb3a7692ce5b9520fb6d389d8ebc16b6e15c4d4
Aarowix <aarowixthesecond@gmail.com>
2025-12-30 13:22:15 +0400

    Birthday

--- getting started with programming/lesson 2 activity 1/Afterclass.py
+++ getting started with programming/lesson 2 activity 1/Afterclass.py
@@ -1,9 +1,13 @@
 # dates of bithdays, format = date/month/year= ش
-Aditya = 10,12,1982
-Neil = 6,1,2012
-Francesco = 1,12,2015
-Evan= 6,5,2010
-Inko = 27,8,1981
-a = print(input("Hi! what is your name : "))
+Cody = 1843
+Python = 20,2,1991
+Java = 23,5,1995
+Scratch = 15,5,2007
+
+a = str(input("Hi! what is your name : "))
 print("Hi",a,"My name is cody") 
 print(input("when were you born? : "))
+print("great! I was born on 1843 when Ada Lovelace wrote me in the first ever computer!", "\nMy friends are Python, Java, and Scratch, they are tools used for programming")
+print(input("Do you know any pragramming platforms? : "))
+print("thats great!")
+print("Python my friend was made in", Python , "Java my friend was made in", Java, "and last but definitely not the least scratch was made in", Scratch )
