c377c6e1bb39cd69d666e69fb7cf301fa94873f0
Aarowix <aarowixthesecond@gmail.com>
2025-12-30 12:13:15 +0400

    Average_of_trees

---
+++ Lesson 4, pyton operators-1/Average.py
@@ -0,0 +1,11 @@
+spruce = 40
+oak = 90
+pine = 100
+birch = 120
+cherry_blossom = 60
+
+sum = spruce+oak+pine+birch+cherry_blossom
+print("sum of all trees is : " ,sum)
+
+average = sum/5
+print("average of trees height : " ,average)
--- getting started with programming/lesson 2 activity 1/Afterclass.py
+++ getting started with programming/lesson 2 activity 1/Afterclass.py
@@ -1,7 +1,9 @@
-# dates of bithdays, format = date/month/year
-a = 10,12,1982
-b = 6,1,2012
-c = 1,12,2015
-d = 6,5,2010
-e = 27,8,1981
-print(a,"\n", b,"\n",c,"\n",d ,"\n", e )
+# dates of bithdays, format = date/month/year= ش
+Aditya = 10,12,1982
+Neil = 6,1,2012
+Francesco = 1,12,2015
+Evan= 6,5,2010
+Inko = 27,8,1981
+a = print(input("Hi! what is your name : "))
+print("Hi",a,"My name is cody") 
+print(input("when were you born? : "))
