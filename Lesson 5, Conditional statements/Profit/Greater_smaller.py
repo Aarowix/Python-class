d894450521eb86e6902af3e46885221ee7ab6950
Aarowix <aarowixthesecond@gmail.com>
2025-12-31 12:40:25 +0400

    Greater_lesser_statements

---
+++ Lesson 5, Conditional statements/Profit/Greater_smaller.py
@@ -0,0 +1,9 @@
+I = int(input("Please enter number : "))
+if I < 15:
+    print("I am Lesser than 15")
+    print("I am an if block")
+else:
+    print("I am greater than 15")
+    print("I am an else block")
+
+print("I am in neither If or Else Block")
