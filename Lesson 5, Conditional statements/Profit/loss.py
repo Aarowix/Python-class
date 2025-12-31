7074ff4a20cb2fdcdd385702dde5af381349a83d
Aarowix <aarowixthesecond@gmail.com>
2025-12-31 12:32:49 +0400

    Proft_loss_statement

---
+++ Lesson 5, Conditional statements/Profit/loss.py
@@ -0,0 +1,9 @@
+W_A = float(input("Please enter actual amount : "))
+S_A = float(input("Please enter sale amount : "))
+if (W_A < S_A):
+    Prof = W_A-S_A
+    print("Profit amount = {0}".format(Prof))
+
+else:
+      print("No profit!!")
+   
