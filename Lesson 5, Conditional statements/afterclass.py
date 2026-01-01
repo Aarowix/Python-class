74f4eaf28f5e3e4f42e6fbf574c9b8c5e96bd2c8
Aarowix <aarowixthesecond@gmail.com>
2026-01-01 11:25:32 +0400

    Outfit_decider

---
+++ Lesson 5, Conditional statements/afterclass.py
@@ -0,0 +1,7 @@
+temp = float(input("Please enter the temprature : "))
+print("Checking for appropriate outfit")
+if temp>30:
+    print("It is hot, \nand is most advised to wear an light and soft outfit") 
+
+else:
+    print("It is cold, \nand it is most advised to wear a warm and heavy outfit")
