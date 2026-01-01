7896c874f0fd7de2f03500436fc002fde3658ab6
Aarowix <aarowixthesecond@gmail.com>
2026-01-01 12:25:10 +0400

    operator_precedence

--- Lesson 6, Python challenges/Operator_precedence.py
+++ Lesson 6, Python challenges/Operator_precedence.py
@@ -3,3 +3,12 @@ b = 30
 c = 28
 d = 0
 print("Value of (b-a)-c " , d)
+
+
+name = "Alex"
+age = 0
+
+if name == "Alex"  or name == "John" and age >=2:
+    print("Hello,welcome")
+else:
+    print("goodbye!!")
