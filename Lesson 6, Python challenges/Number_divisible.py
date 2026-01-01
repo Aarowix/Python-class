d075d9eb4b36ef7fa7c1160857da8e678dbc0d5d
Aarowix <aarowixthesecond@gmail.com>
2026-01-01 12:37:33 +0400

    Divisable_number

---
+++ Lesson 6, Python challenges/Number_divisible.py
@@ -0,0 +1,6 @@
+numn = int(input("Please enter  the Numerator : "))
+numd = int(input("Please enter Denominater : "))
+if numn%numd==0:
+    print("\n" +str(numn)+ "is divisable by"  +str(numd))
+else:
+    print("\n" +str(numn)+ "is not divisable by" +str(numd))
