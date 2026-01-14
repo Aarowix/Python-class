3aee828abeaf7eddfa0225107f1304218b85c4d7
Aarowix <aarowixthesecond@gmail.com>
2026-01-14 20:20:54 +0400

    age_applicationbs

---
+++ INTM_classes/first_class.py/afterclass.py
@@ -0,0 +1,7 @@
+age = (int(input("Please enter your age :")))
+if age >=10:
+    print("You can join the session!")
+if age > 20:
+    print(" Sorry you are not the appropriate age for this session")
+if age<10:
+   print("Sorry you are not the appropriate age for this session")
