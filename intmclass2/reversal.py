c8ad111d2564ed901a44fa8784ebee86bc8c50f9
Aarowix <aarowixthesecond@gmail.com>
2026-01-17 13:03:17 +0400

    reversal

---
+++ intmclass2/reversal.py
@@ -0,0 +1,6 @@
+string = input("Please enter your string for reversal : ")
+string2 = ('')
+for i in string:
+    string2 = i+string2
+print("\nHere is the original string :", string)
+print("Here is the reversed string : ",string2 )
